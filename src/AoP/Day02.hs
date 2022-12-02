96fd19da22583061e20a3f88a2eb8e9752989a8b
Sebastián Aedo <sebastian.aedo29@gmail.com>
2022-12-02 17:54:34 -0300

    reduced logic

--- src/AoP/Day02.hs
+++ src/AoP/Day02.hs
@@ -4,15 +4,28 @@ data Throw
     = Rock
     | Paper
     | Scissor
-    deriving (Show)
+    deriving (Show, Eq)
 
 data Outcome
-  = Player
-  | Enemy
+  = Win
+  | Loss
   | Tie
   deriving (Show)
 
 type Round = (Outcome, Throw)
+
+is_winner :: (Throw, Throw) -> Round
+is_winner (enemy, you)
+  = let
+      outcome = case (enemy, you) of
+        (Rock, Scissor)  -> Loss
+        (Paper, Rock)    -> Loss
+        (Scissor, Paper) -> Loss
+        _                ->
+          if enemy == you then Tie
+          else Win
+    in
+      (outcome, you)
 
 parse_throw :: String -> Throw
 parse_throw s = 
@@ -20,7 +33,7 @@ parse_throw s =
     "A" -> Rock
     "B" -> Paper
     "C" -> Scissor
-  -- Player choices:
+  -- Win choices:
     "X" -> Rock
     "Y" -> Paper
     "Z" -> Scissor
@@ -28,23 +41,7 @@ parse_throw s =
 
 parse_match :: [String] -> (Throw, Throw)
 parse_match [a, b] = (parse_throw a, parse_throw b)
-
 parse_match _ = error "Wrong input"
-
-resolve_match :: (Throw, Throw) -> Round
-resolve_match throw = 
-  let outcome = case throw of
-       (Rock,      Paper)     -> Player
-       (Rock,      Scissor)   -> Enemy
-       (Rock,      Rock)      -> Tie
-       (Paper,     Paper)     -> Tie
-       (Paper,     Scissor)   -> Player
-       (Paper,     Rock)      -> Enemy
-       (Scissor,   Paper)     -> Enemy
-       (Scissor,   Scissor)   -> Tie
-       (Scissor,   Rock)      -> Player
-  in
-       (outcome, snd throw)
 
 calculate_score :: Round -> Int
 calculate_score (outcome, throw) = 
@@ -54,18 +51,15 @@ calculate_score (outcome, throw) =
         Scissor  -> 3
   in
     throw_score + case outcome of
-        Player -> 6
+        Win -> 6
         Tie    -> 3
-        Enemy  -> 0
-
-parse_all_matches :: String -> [(Throw, Throw)]
-parse_all_matches s = map parse_match $ map words $ lines s
+        Loss  -> 0
 
 solve1 :: String -> Int
 solve1 s =
-  let all_matches = parse_all_matches s
+  let all_matches = map parse_match $ map words $ lines s
   in
-    foldr (\ x y -> (calculate_score $ resolve_match x) + y ) 0 all_matches
+    foldr (\ x y -> (calculate_score $ is_winner x) + y ) 0 all_matches
   
 -- second part
 parse_match2 :: [String] -> (Throw, Outcome)
@@ -75,23 +69,24 @@ parse_match2 _ = error "Bad input"
 parse_outcome :: String -> Outcome
 parse_outcome s = 
   case s of
-    "X" -> Enemy
+    "X" -> Loss
     "Y" -> Tie
-    "Z" -> Player
+    "Z" -> Win
     _ -> error "Bad input"
 
 resolve_match2 :: (Throw, Outcome) -> Round
 resolve_match2 (enemy_throw, outcome) =
-  let throw = case (enemy_throw, outcome) of
-       (Rock,      Player)  -> Paper
-       (Rock,      Enemy)   -> Scissor
-       (Rock,      Tie)     -> Rock
-       (Paper,     Tie)     -> Paper
-       (Paper,     Player)  -> Scissor
-       (Paper,     Enemy)   -> Rock
-       (Scissor,   Enemy)   -> Paper
-       (Scissor,   Tie)     -> Scissor
-       (Scissor,   Player)  -> Rock
+  let
+    throw = case (enemy_throw, outcome) of
+       (Rock,      Win)   -> Paper
+       (Rock,      Loss)  -> Scissor
+       (Rock,      Tie)   -> Rock
+       (Paper,     Tie)   -> Paper
+       (Paper,     Win)   -> Scissor
+       (Paper,     Loss)  -> Rock
+       (Scissor,   Loss)  -> Paper
+       (Scissor,   Tie)   -> Scissor
+       (Scissor,   Win)   -> Rock
   in
        (outcome, throw)
     
